f3ad578589e92935a79d02fcc96504db1839311c
Garrett Tailby <lastgameforlife@hotmail.com>
2019-10-11 15:29:47 -0700

    Json Reader.

--- app/src/main/java/com/example/parkingstallfinder/MapsActivity.java
+++ app/src/main/java/com/example/parkingstallfinder/MapsActivity.java
@@ -3,13 +3,18 @@ package com.example.parkingstallfinder;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.example.parkingstallfinder.Util.UtilJson;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -23,14 +28,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -40,8 +45,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng van = new LatLng(49.2488125, -122.9805344);
+        mMap.addMarker(new MarkerOptions().position(van).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(van));
+        try{
+            UtilJson uJ = new UtilJson(this);
+            String input = uJ.readJSONFromAsset("parking_meters_van.json");
+            JSONArray jsArray = new JSONArray(input);
+            Log.e("JSON", jsArray.toString());
+        }catch(Exception e){
+            Log.e("Error", e.getLocalizedMessage());
+        }
+
     }
 }
